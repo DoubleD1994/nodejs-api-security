172c25bd1fc67b1fccfc67d1ce9f2f764422fee7
ddr14 <david.dryburgh@sky.uk>
2020-07-03 17:52:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
+
+const app = express();
+const PORT = 3000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
